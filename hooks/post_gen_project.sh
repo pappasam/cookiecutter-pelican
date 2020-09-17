8ed4ec2d7b008d843fb5254fbebfe492e07b79c4
Sam Roeca <samuel.roeca@gmail.com>
2020-09-17 16:16:21 -0700

    Make all python commands use "poetry run"

--- hooks/post_gen_project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "***************************************************"
-echo "* Success! Some next steps:"
-echo "***************************************************"
-echo "* 1. Create & activate a Python virtual environment"
-echo "* 2. Run 'make setup'"
-echo "***************************************************"
